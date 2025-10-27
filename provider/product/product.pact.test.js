29d6024534119f43a25433b789b0348cfb6ecc33
Benedek Szabó-B <battancs.benedek@gmail.com>
2025-10-27 22:40:25 +0100

    prep

--- provider/product/product.pact.test.js
+++ provider/product/product.pact.test.js
@@ -1,5 +1,7 @@
 const { Verifier } = require('@pact-foundation/pact');
 const path = require('path');
+const Product = require('./product')
+const controller = require('./product.controller')
 
 // Setup provider server to verify
 const app = require('express')();
@@ -15,7 +17,20 @@ describe("Pact Verification", () => {
             providerVersion: "1.0.0",
             pactUrls: [
                 path.resolve(__dirname, '../../consumer/pacts/FrontendWebsite-ProductService.json')
-            ]
+            ],
+            stateHandlers: {
+                "product with ID 10 exists": ()=>{
+                    controller.repository.products = new Map([
+                        ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+                    ])
+                },
+                "products exist": () => {
+                    controller.repository.products = new Map([
+                        ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
+                        ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+                    ])
+                }
+            }
         };
 
         return new Verifier(opts).verifyProvider().then(output => {
